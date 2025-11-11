ed1b79ea774e15c65340fcd5aa4cf32c61f9a22a
Viktor De Pasquale <hello@depasquale.wiki>
2025-11-11 21:31:54 +0100

    feat(backup): add webhook notification for backup status
    - Introduced webhook option for HTTP POST notifications on backup start and completion
    - Added error handling and HTTP POST request implementation
    - Enhanced feedback mechanism through webhook integration

--- agent/src/main/kotlin/com/copycrane/agent/cli/Backup.kt
+++ agent/src/main/kotlin/com/copycrane/agent/cli/Backup.kt
@@ -5,6 +5,9 @@ import com.github.ajalt.clikt.parameters.options.help
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.file
+import org.intellij.lang.annotations.Language
+import java.net.HttpURLConnection
+import java.net.URI
 
 class Backup : CliktCommand() {
     private val source by option("-i")
@@ -17,45 +20,88 @@ class Backup : CliktCommand() {
     private val mountPoint by option("-m")
         .file(canBeDir = true, canBeFile = false, canBeSymlink = true)
         .help("Mount point for remote destination")
+    private val webhook by option("-w")
+        .required()
+        .help("Webhook URL to send HTTP POST request to on backup start and end")
 
     override fun run() {
-        with(Runtime.getRuntime()) {
-            val mountPoint = mountPoint
-            if (mountPoint != null) {
-                if (exists("findmnt")) {
-                    val p = exec(arrayOf("findmnt", "-M", "${mountPoint.canonicalFile}"))
-                    p.inputReader().forEachLine {
-                        println(it)
+        try {
+            with(Runtime.getRuntime()) {
+                val mountPoint = mountPoint
+                if (mountPoint != null) {
+                    if (exists("findmnt")) {
+                        val p = exec(arrayOf("findmnt", "-M", "${mountPoint.canonicalFile}"))
+                        p.inputReader().forEachLine {
+                            println(it)
+                        }
+                        check(p.waitFor() == 0) {
+                            "Mount point was specified and is not mounted"
+                        }
+                    } else {
+                        System.err.println("findmnt not found, skipping mount point check(!)")
                     }
-                    check(p.waitFor() == 0) {
-                        "Mount point was specified and is not mounted"
-                    }
-                } else {
-                    System.err.println("findmnt not found, skipping mount point check(!)")
+                }
+                check(exists("rsync")) {
+                    "rsync not found"
+                }
+
+                val p = exec(
+                    arrayOf(
+                        "rsync",
+                        "-avh",
+                        "--delete",
+                        "--progress",
+                        "${source.canonicalFile}",
+                        destination
+                    )
+                )
+                p.inputReader().forEachLine {
+                    println(it)
+                }
+                check(p.waitFor() == 0) {
+                    "Rsync failed, see output"
                 }
             }
-            check(exists("rsync")) {
-                "rsync not found"
-            }
-
-            val p = exec(
-                arrayOf(
-                    "rsync",
-                    "-avh",
-                    "--delete",
-                    "--progress",
-                    "${source.canonicalFile}",
-                    destination
-                )
-            )
-            p.inputReader().forEachLine {
-                println(it)
-            }
-            check(p.waitFor() == 0) {
-                "Rsync failed, see output"
-            }
+            send("✅ Backup done", true)
+        } catch (e: Throwable) {
+            send("⚠️ Backup failed", false)
+            throw e
         }
     }
 
     fun exists(program: String) = Runtime.getRuntime().exec(arrayOf("which", program)).waitFor() == 0
+
+    private fun escapeJson(value: String): String {
+        return value.replace("\\", "\\\\").replace("\"", "\\\"")
+    }
+
+    fun send(message: String, success: Boolean) {
+        URI(webhook).toURL().openConnection().apply {
+            this as HttpURLConnection
+            requestMethod = "POST"
+            doOutput = true
+            setRequestProperty("Content-Type", "application/json; charset=utf-8")
+            outputStream.writer(Charsets.UTF_8).use {
+                val input = escapeJson(source.path)
+                val output = escapeJson(destination)
+                val color = if (success) "good" else "danger"
+                @Language("JSON")
+                val text = """{
+                        "attachments": [{
+                            "title": "$message",
+                            "color": "$color",
+                            "fields": [
+                                {"title": "Input", "value": "$input", "short": false},
+                                {"title": "Output", "value": "$output", "short": false}
+                            ]
+                        }]
+                    }""".trimIndent()
+                it.write(text)
+            }
+            if (responseCode >= 300) {
+                val error = errorStream?.reader()?.readText()
+                System.err.println("Webhook failed: $responseCode $responseMessage\n$error")
+            }
+        }
+    }
 }
